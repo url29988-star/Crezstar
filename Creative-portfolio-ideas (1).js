4830a9f90b06e5d7ddb16b9cbc74b7f27f2fbf3a
url29988-star <url29988@gmail.com>
2026-07-03 12:35:59 +0530

    CREZSTAR site

---
+++ Creative-portfolio-ideas (1).js
@@ -0,0 +1,19 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// REPLACE THIS WITH YOUR CONFIG FROM FIREBASE CONSOLE
+const firebaseConfig = {
+  apiKey: "AIzaSyAFJwXkxOMz3vnwYDPj4GcIQplEWz5H4C0",
+  authDomain: "my-sky-c35b8.firebaseapp.com",
+  projectId: "my-sky-c35b8",
+  storageBucket: "my-sky-c35b8.firebasestorage.app",
+  messagingSenderId: "219045133103",
+  appId: "1:219045133103:web:540b5ee451b64725c8ffda",
+  measurementId: "G-LSQLJ7BYKW"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
